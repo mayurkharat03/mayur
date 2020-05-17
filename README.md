76df8d0b62fac3e949e5cacf7829c30e2cf652b2
mayurapptware <mayur.kharat@apptware.com>
2020-05-17 17:31:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# What is this?
+
+Sample example of module export.
+
+#Installation
+
+`npm install mayur --save`
---
+++ index.js
@@ -0,0 +1,8 @@
+function npmTestHello(){
+    let a=3;
+    let b=2;
+    alert('Hello NPM Test');
+    console.log('Test',a+b);
+    
+}
+module.exports.npmTestHello = npmTestHello;
